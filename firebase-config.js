7e022a758cb0dd9ca74c1939f3544657a97e742e
Vikram34it <vikramkumar.viva@gmail.com>
2026-04-24 19:17:06 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+window.COUPON_TRACKER_FIREBASE = {
+  enabled: true,
+  config: {
+    apiKey: "AIzaSyD8dSCMn4aB2ltinbkDzzuRQR6I1gI_D0M",
+    authDomain: "coupons-tracker.firebaseapp.com",
+    databaseURL: "",
+    projectId: "coupons-tracker",
+    storageBucket: "coupons-tracker.firebasestorage.app",
+    messagingSenderId: "342199257416",
+    appId: "1:342199257416:web:a83c8d1a1133716dc70b7d",
+    measurementId: "G-0RYVJV1DLS"
+  },
+  databasePath: "couponTracker/appState"
+};
